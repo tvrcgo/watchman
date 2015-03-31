eb3d3aa61ffee17f60463571cdcfe53301a04b94
tvrcgo <tvrcgo@gmail.com>
2015-03-31 13:03:57 +0800

    support: bind multiple invokes on one point.

--- README.md
+++ README.md
@@ -2,11 +2,28 @@
 监视服务器运行情况
 
 ## Usage
+监视指定的主机端口运行是否正常，不指定主机时监听本机端口。
+
 ```js
 var man = Watchman();
-man.task(80, 'localhost');
-man.task(27017, 'example.com', function(point){
-	console.log(point);
+man.watch(80, function(point){
+	// localhost:80 state change
+});
+man.watch(27017, 'example.com', function(point){
+	// example.com:27017 state change
+});
+man.start(3000); // check interval 3000ms
+```
+
+同一个主机端口可绑定多个回调。
+
+```js
+var man = Watchman();
+man.watch(80, 'example.com', function(point){
+	// example.com:80 state change handler
+});
+man.watch(80, 'example.com', function(point){
+	// example.com:80 state change handler 2
 });
 man.start(3000);
 ```
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var net = require('net');
 var EventProxy = require('eventproxy');
 
+module.exports = Watchman;
+
 function Watchman(){
 
 	if(!(this instanceof Watchman)) { 
@@ -9,48 +11,60 @@ function Watchman(){
 	}
 
 	this._man = new EventProxy();
-	this._task = [], this._timer;
+	this._task = {}, this._timer;
+	var pointStatus = {}, _this = this;
 
 	this._man.on('ok', function(point){
-		console.log(point);
+		var _point = point.host+':'+point.port;
+		if (pointStatus[_point] === false) {
+			_this._task[_point].forEach(function(fn){
+				fn(point);
+			});
+		}
+		pointStatus[_point] = true;
 	});
 
 	this._man.on('fail', function(point){
-		console.log(point);
+		var _point = point.host+':'+point.port;
+		if (pointStatus[_point] === undefined || pointStatus[_point]) {
+			_this._task[_point].forEach(function(fn){
+				fn(point);
+			});
+		}
+		pointStatus[_point] = false;
 	});
 }
 
 Watchman.prototype = {
 
 	watch: function(port, host, fn){
-		host = host || "127.0.0.1";
+
+		if ( typeof host === 'function' ) {
+			fn = host;
+			host = "127.0.0.1";
+		}
+
 		fn = fn || function(){};
-		this._task.push([port, host, fn])
+		var _point = host+':'+port;
+		if (!this._task[_point]) { this._task[_point] = []; }
+		this._task[_point].push(fn);
 	},
 
-	check: function(PORT, HOST, fn){
+	check: function(POINT){
+
+		POINT = POINT.split(':');
+		var HOST = POINT[0], PORT = POINT[1];
 
 		var client = new net.Socket();
 		var _this = this;
 
 		client.connect(PORT, HOST, function(){
-			_this._man.emit('ok', { host:HOST, port:PORT, status:'connected' });
+			_this._man.emit('ok', { host:HOST, port:PORT, status:'OK' });
 			client.destroy();
 		});
 
 		client.on('error', function(err){
-			switch(err.code) {
-				case "ECONNREFUSED" :
-					_this._man.emit('fail', { host:HOST, port:PORT, status:'refuse' });
-					fn();
-					break;
-				case "ENOTFOUND" :
-					_this._man.emit('fail', { host:HOST, port:PORT, status:'host not found' });
-					fn();
-					break;
-				default:
-					console.log(err);
-			}
+			_this._man.emit('fail', { host:HOST, port:PORT, status:'FAIL', err:err.code });
 			client.destroy();
 		});
 	},
@@ -58,9 +72,9 @@ Watchman.prototype = {
 	start: function(interval){
 		var _this = this;
 		this._timer = setInterval(function(){
-			_this._task.forEach(function(point){
-				_this.check.apply(_this, point);
-			});
+			for (var point in _this._task) {
+				_this.check(point);
+			}
 		}, interval||5000);
 	},
 
